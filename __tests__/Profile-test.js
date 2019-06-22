2b63786aea301ff1b79cf25038c7e72704caf329
Bar Azouri <barazouri24@gmail.com>
2019-06-22 22:22:27 +0300

    eslint

--- __tests__/Profile-test.js
+++ __tests__/Profile-test.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import Profile from '../src/components/Profile/Profile'
-
-//TODO: fetch is not defined in nodejs
-// so we are checking globally if fetch exists
-// This code snippet should be moved to a generic place for test setup.
-// see Jest documentation of how to do that
 if (!global.fetch) {
   global.fetch = require('node-fetch')
 }
